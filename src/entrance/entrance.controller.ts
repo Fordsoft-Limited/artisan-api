b41138b974941f11b8e5e784ce10e1f1cc44ac37
odofin-timothy <odofintimothy@gmail.com>
2023-11-11 16:34:25 +0100

    Added response code

--- src/entrance/entrance.controller.ts
+++ src/entrance/entrance.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
 import { EntranceService } from './entrance.service';
 import { LoginRequest, LoginResponse } from './model/login.model';
 import { ArtisanApiResponse } from 'src/model/app.model';
+import { ErrorCode } from 'src/utils/app.util';
 
 @Controller('entrance')
 export class EntranceController {
     constructor(private entranceService: EntranceService){}
 
     @Post("login")
+    @HttpCode(ErrorCode.HTTP_200)
     async login(@Body() payload: LoginRequest):Promise<ArtisanApiResponse>{
         return await this.entranceService.login(payload)
     }
