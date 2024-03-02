af3087ef6e7aab72f56d86f362438a2975b2b5ef
odofin-timothy <odofintimothy@gmail.com>
2024-03-02 13:30:17 +0100

    Added cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { EntranceModule } from './entrance/entrance.module';
 import { AdminModule } from './admin/admin.module';
 import { NotificationModule } from './notification/notification.module';
-import { ConfigModule } from '@nestjs/config';
 import { MediaModule } from './media/media.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './global/database/database.module';
-import SecretConfig from './config/secret.config'
 import { AdminController } from './admin/admin.controller';
 import { GetUserMiddleware } from './middleware/get-user.middleware';
 import { ConversationController } from './conversation/conversation.controller';
 import { UploadModule } from './upload/upload.module';
+
 @Module({
   imports: [
     DatabaseModule,
@@ -21,7 +20,8 @@ import { UploadModule } from './upload/upload.module';
     NotificationModule,
     MediaModule,
     ConversationModule,
-    UploadModule],
+    UploadModule
+  ],
 
 })
 export class AppModule implements NestModule {
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1");
 
+  app.enableCors({
+    origin: ["http://localhost:5000","http://localhost:4200","http://localhost:8080"], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle("Artisan Web Service")
     .setDescription("A set of API for management of the artisan website and administrative dashboard")
