4032e63cd6d6b4893fbfc8bdbb72541de7c4c742
odofin-timothy <odofintimothy@gmail.com>
2024-03-12 09:44:13 +0100

    Updated deployment script

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const DEFAULT_PAGE:number=1;
 export const DEFAULT_SIZE:number=50
 export const MAIL_FROM = 'notification@getswipe.org';
 export const VALID_NIGERIAN_PHONE_NUMBER_REGEX = /^(?:(?:(?:\+?234(?:\h1)?|01)\h*)?(?:\(\d{3}\)|\d{3})|\d{4})(?:\W*\d{3})?\W*\d{4}(?!\d)$/
-export const FILE_UPLOAD_DIRECTORY='/home/artisan-api/public_html/server/uploads/'
+export const FILE_UPLOAD_DIRECTORY='/home/deployuser/server/uploads/'
 
 
 
