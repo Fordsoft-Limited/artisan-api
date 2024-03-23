b6596d91ece2a8cd5ad0095f885324ec9352b43a
odofin-timothy <odofintimothy@gmail.com>
2024-03-23 10:19:52 +0100

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api/v1");
 
   app.enableCors({
-    origin: ["http://localhost:5000","http://localhost:4200","http://localhost:8080"], 
+    origin: ["http://localhost:5000","http://localhost:4200","http://localhost:3000"], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
