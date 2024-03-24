ddc9d06b8d8085478203a49f5610719538f90954
odofin-timothy <odofintimothy@gmail.com>
2024-03-24 22:15:34 +0100

    add cors for the admin portal

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api/v1");
 
   app.enableCors({
-    origin: ["http://localhost:5000","http://localhost:4200","http://localhost:3000"], 
+    origin: ["http://localhost:5000","http://localhost:4200","http://localhost:3000","http://admin-artisan.fordsoft.tech"], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
